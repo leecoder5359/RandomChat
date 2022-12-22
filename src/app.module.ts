71d34d01ceecf34b0443501fe20187095677a27c
leecoder <hj.lee@explzn.com>
2022-12-22 09:52:40 +0900

    (init): socket boiler-templete

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ChatsModule } from './chats/chats.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }),
+  MongooseModule.forRoot(process.env.MONGODB_URI2, {
+    useNewUrlParser: true,
+  }),
+  ChatsModule,
+],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
-export class AppModule {}
+
+export class AppModule implements NestModule {
+  private readonly isDev: boolean = process.env.MODE === 'dev' ? true : false;
+
+  configure() {
+    mongoose.set('debug', this.isDev);
+  }
+}
